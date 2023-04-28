42f8c232fa17419542bb75414c31a966381b7bd6
Nayaka90 <113663282+Nayaka90@users.noreply.github.com>
2023-04-28 13:12:11 +0530

    Update firstjava.java
    mm

--- src/test/java/github/firstjava.java
+++ src/test/java/github/firstjava.java
@@ -5,9 +5,11 @@ public class firstjava {
 	public static void main(String[] args) {
 		System.out.println("hi");
 		System.out.println("hi");
+		
+	System.out.println("kk");
 		System.out.println("hi");
 
-	
+	System.out.println("kk");
 System.out.println("hi");
 		System.out.println("hi");
 		System.out.println("hi");
